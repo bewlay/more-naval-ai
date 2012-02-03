0c2270772092875856849a27c91a98efe2acbfc0
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2012-02-03 17:15:28 +0000

    Code - fixing a typo

--- CvGameCoreDLL/CyEnumsInterface.cpp
+++ CvGameCoreDLL/CyEnumsInterface.cpp
@@ -790,7 +790,7 @@ void CyEnumsPythonInterface()
 // BUG - start
 		.value("CITIZEN_CHAR", CITIZEN_CHAR)
 		.value("GREAT_GENERAL_CHAR", GREAT_GENERAL_CHAR)
-		.value("AIRPORT_CHAR", GREAT_GENERAL_CHAR)
+		.value("AIRPORT_CHAR", AIRPORT_CHAR)
 // BUG - end
 		.value("MAX_NUM_SYMBOLS", MAX_NUM_SYMBOLS)
 		;
