0c3bfeccec05439b35037c71c2e6df133af99933
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2012-02-06 17:21:20 +0000

    UI - Sevopedia updates for FFH (code by Denev - taken from Wild Mana)

--- python/Contrib/Sevopedia/SevoPediaCivilization.py
+++ python/Contrib/Sevopedia/SevoPediaCivilization.py
@@ -14,6 +14,9 @@ from CvPythonExtensions import *
 import CvUtil
 import ScreenInput
 import SevoScreenEnums
+##--------	BUGFfH: Added by Denev 2009/08/14
+import SevoPediaMain
+##--------	BUGFfH: End Add
 
 gc = CyGlobalContext()
 ArtFileMgr = CyArtFileMgr()
@@ -25,50 +28,87 @@ class SevoPediaCivilization:
 		self.iCivilization = -1
 		self.top = main
 
+##--------	BUGFfH: Modified by Denev 2009/10/08
+		X_MERGIN = self.top.X_MERGIN
+		Y_MERGIN = self.top.Y_MERGIN
+
+		self.W_CIV_TRAIT = 150
+		self.H_CIV_TRAIT = 116
+		self.X_CIV_TRAIT = self.top.R_PEDIA_PAGE - self.W_CIV_TRAIT
+		self.Y_CIV_TRAIT = self.top.Y_PEDIA_PAGE
+
+		self.W_TECHS = 180
+		self.H_TECHS = self.H_CIV_TRAIT
+		self.X_TECHS = self.X_CIV_TRAIT - self.W_TECHS - X_MERGIN
+		self.Y_TECHS = self.Y_CIV_TRAIT
+
 		self.X_MAIN_PANE = self.top.X_PEDIA_PAGE
-		self.Y_MAIN_PANE = self.top.Y_PEDIA_PAGE
+		self.Y_MAIN_PANE = self.Y_CIV_TRAIT
+		self.W_MAIN_PANE = self.X_TECHS - self.X_MAIN_PANE - X_MERGIN
+		self.H_MAIN_PANE = self.H_CIV_TRAIT
 
-		self.Y_TECH = self.Y_MAIN_PANE
-		self.H_TECH = 110
-
-		self.Y_LEADER = self.Y_TECH + self.H_TECH + 10
-		self.H_LEADER = 110
-
-		self.H_MAIN_PANE = self.Y_LEADER + self.H_LEADER - self.Y_MAIN_PANE
-		self.W_MAIN_PANE = self.H_MAIN_PANE
-
-		self.X_TECH = self.X_MAIN_PANE + self.W_MAIN_PANE + 10
-		self.W_TECH = self.top.R_PEDIA_PAGE - self.X_TECH
-
-		self.X_LEADER = self.X_TECH
-		self.W_LEADER = self.W_TECH
-
-		self.W_ICON = 150
-		self.H_ICON = 150
+		self.W_ICON = 100
+		self.H_ICON = 100
 		self.X_ICON = self.X_MAIN_PANE + (self.H_MAIN_PANE - self.H_ICON) / 2
 		self.Y_ICON = self.Y_MAIN_PANE + (self.H_MAIN_PANE - self.H_ICON) / 2
 		self.ICON_SIZE = 64
 
-		self.X_BUILDING = self.X_MAIN_PANE
-		self.Y_BUILDING = self.Y_LEADER + self.H_LEADER + 10
-		self.W_BUILDING = 130
-		self.H_BUILDING = 110
+		self.X_HEROES = self.X_MAIN_PANE
+		self.Y_HEROES = self.Y_MAIN_PANE + self.H_MAIN_PANE + Y_MERGIN
+		self.W_HEROES = 155
+		self.H_HEROES = 110
 
-		self.X_UNIT = self.X_BUILDING + self.W_BUILDING + 10
-		self.Y_UNIT = self.Y_BUILDING
-		self.W_UNIT = self.top.R_PEDIA_PAGE - self.X_UNIT
-		self.H_UNIT = 110
+		self.X_SPELLS = self.X_HEROES + self.W_HEROES + X_MERGIN
+		self.Y_SPELLS = self.Y_HEROES
+		self.W_SPELLS = 155
+		self.H_SPELLS = self.H_HEROES
+
+		self.X_LEADERS = self.X_SPELLS + self.W_SPELLS + X_MERGIN
+		self.Y_LEADERS = self.Y_HEROES
+		self.W_LEADERS = self.top.R_PEDIA_PAGE - self.X_LEADERS
+		self.H_LEADERS = self.H_HEROES
+
+		self.X_BUILDINGS = self.X_MAIN_PANE
+		self.Y_BUILDINGS = self.Y_HEROES + self.H_HEROES + Y_MERGIN
+		self.W_BUILDINGS = 215
+		self.H_BUILDINGS = self.H_HEROES
+
+		self.X_UNITS = self.X_BUILDINGS + self.W_BUILDINGS + X_MERGIN
+		self.Y_UNITS = self.Y_BUILDINGS
+		self.W_UNITS = self.top.R_PEDIA_PAGE - self.X_UNITS
+		self.H_UNITS = self.H_BUILDINGS
+
+		self.X_EX_BUILDINGS = self.X_BUILDINGS
+		self.Y_EX_BUILDINGS = self.Y_BUILDINGS + self.H_BUILDINGS + Y_MERGIN
+		self.W_EX_BUILDINGS = self.W_BUILDINGS
+		self.H_EX_BUILDINGS = self.H_BUILDINGS
+
+		self.X_EX_UNITS = self.X_UNITS
+		self.Y_EX_UNITS = self.Y_EX_BUILDINGS
+		self.W_EX_UNITS = self.W_UNITS
+		self.H_EX_UNITS = self.H_UNITS
 
 		self.X_TEXT = self.X_MAIN_PANE
-		self.Y_TEXT = self.Y_BUILDING + self.H_BUILDING + 10
+		self.Y_TEXT = self.Y_EX_BUILDINGS + self.H_EX_BUILDINGS + 10
 		self.W_TEXT = self.top.R_PEDIA_PAGE - self.X_TEXT
 		self.H_TEXT = self.top.B_PEDIA_PAGE - self.Y_TEXT
+
+		self.Y_WIDE_TEXT = self.Y_EX_BUILDINGS
+		self.H_WIDE_TEXT = self.top.B_PEDIA_PAGE - self.Y_WIDE_TEXT
+##--------	BUGFfH: End Modify
 
 
 
 	def interfaceScreen(self, iCivilization):
 		self.iCivilization = iCivilization
 		screen = self.top.getScreen()
+
+##--------	BUGFfH: Added by Denev 2009/08/16
+		# Header...
+		szHeader = u"<font=4b>" + gc.getCivilizationInfo(self.iCivilization).getDescription() + u"</font>"
+		szHeaderId = "PediaMainHeader"
+		screen.setText(szHeaderId, "Background", szHeader, CvUtil.FONT_CENTER_JUSTIFY, self.top.X_SCREEN, self.top.Y_TITLE, 0, FontTypes.TITLE_FONT, WidgetTypes.WIDGET_GENERAL, -1, -1)
+##--------	BUGFfH: End Add
 
 		screen.addPanel(self.top.getNextWidgetName(), "", "", False, False, self.X_MAIN_PANE, self.Y_MAIN_PANE, self.W_MAIN_PANE, self.H_MAIN_PANE, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.addPanel(self.top.getNextWidgetName(), "", "", False, False, self.X_ICON, self.Y_ICON, self.W_ICON, self.H_ICON, PanelStyles.PANEL_STYLE_MAIN)
@@ -78,6 +118,22 @@ class SevoPediaCivilization:
 		self.placeBuilding()
 		self.placeUnit()
 		self.placeLeader()
+
+##--------	BUGFfH: Added by Denev 2009/10/08
+		self.bWideText = true
+
+		self.placeTrait()
+		self.placeHero()
+		self.placeWorldSpell()
+
+		szPanelName1 = self.placeBlockedBuilding()
+		szPanelName2 = self.placeBlockedUnit()
+
+		if self.bWideText:
+			screen.hide(szPanelName1)
+			screen.hide(szPanelName2)
+##--------	BUGFfH: End Add
+
 		self.placeText()
 
 
@@ -85,7 +141,7 @@ class SevoPediaCivilization:
 	def placeTech(self):
 		screen = self.top.getScreen()
 		panelName = self.top.getNextWidgetName()
-		screen.addPanel(panelName, localText.getText("TXT_KEY_FREE_TECHS", ()), "", False, True, self.X_TECH, self.Y_TECH, self.W_TECH, self.H_TECH, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.addPanel(panelName, localText.getText("TXT_KEY_FREE_TECHS", ()), "", False, True, self.X_TECHS, self.Y_TECHS, self.W_TECHS, self.H_TECHS, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.attachLabel(panelName, "", "  ")
 		for iTech in range(gc.getNumTechInfos()):
 			if (gc.getCivilizationInfo(self.iCivilization).isCivilizationFreeTechs(iTech)):
@@ -96,33 +152,97 @@ class SevoPediaCivilization:
 	def placeBuilding(self):
 		screen = self.top.getScreen()
 		panelName = self.top.getNextWidgetName()
-		screen.addPanel(panelName, localText.getText("TXT_KEY_UNIQUE_BUILDINGS", ()), "", False, True, self.X_BUILDING, self.Y_BUILDING, self.W_BUILDING, self.H_BUILDING, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.addPanel(panelName, localText.getText("TXT_KEY_UNIQUE_BUILDINGS", ()), "", False, True, self.X_BUILDINGS, self.Y_BUILDINGS, self.W_BUILDINGS, self.H_BUILDINGS, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.attachLabel(panelName, "", "  ")
 		for iBuilding in range(gc.getNumBuildingClassInfos()):
 			iUniqueBuilding = gc.getCivilizationInfo(self.iCivilization).getCivilizationBuildings(iBuilding)
 			iDefaultBuilding = gc.getBuildingClassInfo(iBuilding).getDefaultBuildingIndex()
+			if iUniqueBuilding != BuildingTypes.NO_BUILDING:			
+				iUniqueCiv = gc.getBuildingInfo(iUniqueBuilding).getPrereqCiv()			
 			if (iDefaultBuilding > -1 and iUniqueBuilding > -1 and iDefaultBuilding != iUniqueBuilding):
 				screen.attachImageButton(panelName, "", gc.getBuildingInfo(iUniqueBuilding).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_BUILDING, iUniqueBuilding, 1, False)
-
+			elif iUniqueCiv != -1 and iUniqueCiv == self.iCivilization:
+				if iUniqueBuilding != BuildingTypes.NO_BUILDING:
+					szButton = gc.getBuildingInfo(iUniqueBuilding).getButton()
+					screen.attachImageButton(panelName, "", szButton, GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_BUILDING, iUniqueBuilding, 1, False)
 
 
 	def placeUnit(self):
 		screen = self.top.getScreen()
 		panelName = self.top.getNextWidgetName()
-		screen.addPanel(panelName, localText.getText("TXT_KEY_FREE_UNITS", ()), "", False, True, self.X_UNIT, self.Y_UNIT, self.W_UNIT, self.H_UNIT, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.addPanel(panelName, localText.getText("TXT_KEY_FREE_UNITS", ()), "", False, True, self.X_UNITS, self.Y_UNITS, self.W_UNITS, self.H_UNITS, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.attachLabel(panelName, "", "  ")
 		for iUnit in range(gc.getNumUnitClassInfos()):
 			iUniqueUnit = gc.getCivilizationInfo(self.iCivilization).getCivilizationUnits(iUnit)
 			iDefaultUnit = gc.getUnitClassInfo(iUnit).getDefaultUnitIndex()
 			if (iDefaultUnit > -1 and iUniqueUnit > -1 and iDefaultUnit != iUniqueUnit):
-				screen.attachImageButton(panelName, "", gc.getUnitInfo(iUniqueUnit).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iUniqueUnit, 1, False)
+##--------	BUGFfH: Modified by Denev 2009/09/05
+#				screen.attachImageButton(panelName, "", gc.getUnitInfo(iUniqueUnit).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iUniqueUnit, 1, False)
+				szUnitButton = gc.getUnitInfo(iUniqueUnit).getUnitButtonWithCivArtStyle(self.iCivilization)
+				screen.attachImageButton(panelName, "", szUnitButton, GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iUniqueUnit, 1, False)
+				continue
+##--------	BUGFfH: End Modify
+
+##--------	BUGFfH: Added by Denev 2009/10/08
+			if iUniqueUnit == UnitTypes.NO_UNIT:
+				continue
+
+			iUniqueCiv = gc.getUnitInfo(iUniqueUnit).getPrereqCiv()
+			if iUniqueCiv == self.iCivilization:
+				szButton = gc.getUnitInfo(iUniqueUnit).getUnitButtonWithCivArtStyle(iUniqueCiv)
+				screen.attachImageButton(panelName, "", szButton, GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iUniqueUnit, 1, False)
+##--------	BUGFfH: End Add
+
+
+
+##--------	BUGFfH: Added by Denev 2009/09/02
+	def placeBlockedBuilding(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_MISC_CIV_BLOCKED_BUILDINGS", ()), "", False, True, self.X_EX_BUILDINGS, self.Y_EX_BUILDINGS, self.W_EX_BUILDINGS, self.H_EX_BUILDINGS, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.attachLabel(panelName, "", "  ")
+
+		for iBuildingClass in range(gc.getNumBuildingClassInfos()):
+			iUniqueBuilding = gc.getCivilizationInfo(self.iCivilization).getCivilizationBuildings(iBuildingClass)
+			iDefaultBuilding = gc.getBuildingClassInfo(iBuildingClass).getDefaultBuildingIndex()
+			if iDefaultBuilding != BuildingTypes.NO_BUILDING:
+				if iUniqueBuilding == BuildingTypes.NO_BUILDING:
+					BuildingInfo = gc.getBuildingInfo(iDefaultBuilding)
+					screen.attachImageButton(panelName, "", BuildingInfo.getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_BUILDING, iDefaultBuilding, 1, False)
+
+					self.bWideText = false
+
+		return panelName
+
+
+
+	def placeBlockedUnit(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_MISC_CIV_BLOCKED_UNITS", ()), "", False, True, self.X_EX_UNITS, self.Y_EX_UNITS, self.W_EX_UNITS, self.H_EX_UNITS, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.attachLabel(panelName, "", "  ")
+
+		for iUnitClass in range(gc.getNumUnitClassInfos()):
+			iUniqueUnit = gc.getCivilizationInfo(self.iCivilization).getCivilizationUnits(iUnitClass)
+			iDefaultUnit = gc.getUnitClassInfo(iUnitClass).getDefaultUnitIndex()
+			if iDefaultUnit != UnitTypes.NO_UNIT:
+				if iUniqueUnit == UnitTypes.NO_UNIT:
+					if not isWorldUnitClass(iUnitClass):
+						if gc.getUnitInfo(iDefaultUnit).getPrereqCiv() == CivilizationTypes.NO_CIVILIZATION:
+							unitInfo = gc.getUnitInfo(iDefaultUnit)
+							screen.attachImageButton(panelName, "", unitInfo.getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iDefaultUnit, 1, False)
+
+							self.bWideText = false
+
+		return panelName
+##--------	BUGFfH: End Add
 
 
 
 	def placeLeader(self):
 		screen = self.top.getScreen()
 		panelName = self.top.getNextWidgetName()
-		screen.addPanel(panelName, localText.getText("TXT_KEY_CONCEPT_LEADERS", ()), "", False, True, self.X_LEADER, self.Y_LEADER, self.W_LEADER, self.H_LEADER, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.addPanel(panelName, localText.getText("TXT_KEY_CONCEPT_LEADERS", ()), "", False, True, self.X_LEADERS, self.Y_LEADERS, self.W_LEADERS, self.H_LEADERS, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.attachLabel(panelName, "", "  ")
 		for iLeader in range(gc.getNumLeaderHeadInfos()):
 			civ = gc.getCivilizationInfo(self.iCivilization)
@@ -134,9 +254,80 @@ class SevoPediaCivilization:
 	def placeText(self):
 		screen = self.top.getScreen()
 		panelName = self.top.getNextWidgetName()
-		screen.addPanel(panelName, "", "", True, True, self.X_TEXT, self.Y_TEXT, self.W_TEXT, self.H_TEXT, PanelStyles.PANEL_STYLE_BLUE50)
+##--------	BUGFfH: Modified by Denev 2009/10/08
+#		screen.addPanel(panelName, "", "", True, True, self.X_TEXT, self.Y_TEXT, self.W_TEXT, self.H_TEXT, PanelStyles.PANEL_STYLE_BLUE50)
+		iY, iH = self.Y_TEXT, self.H_TEXT
+		if self.bWideText:
+			iY, iH = self.Y_WIDE_TEXT, self.H_WIDE_TEXT
+		screen.addPanel( panelName, "", "", true, true, self.X_TEXT, iY, self.W_TEXT, iH, PanelStyles.PANEL_STYLE_BLUE50 )
+##--------	BUGFfH: End Modify
 		szText = gc.getCivilizationInfo(self.iCivilization).getCivilopedia()
 		screen.attachMultilineText(panelName, "Text", szText, WidgetTypes.WIDGET_GENERAL, -1, -1, CvUtil.FONT_LEFT_JUSTIFY)
+
+
+
+##--------	BUGFfH: Added by Denev 2009/08/14
+	def placeTrait(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+
+
+		#terrain yield modifier
+		ltTerrainModifier = []
+#		for iTerrain in range(gc.getNumTerrainInfos()):
+#			if self.iCivilization == gc.getTerrainInfo(iTerrain).getCivilizationYieldType():
+#				szYield = u""
+#				bFirst = true
+#				for iYield in range(YieldTypes.NUM_YIELD_TYPES):
+#					iYieldChange = gc.getTerrainInfo(iTerrain).getCivilizationYieldChange(iYield)
+#					if (iYieldChange != 0):
+#						if not bFirst:
+#							szYield += ", "
+#						bFirst = false
+#
+#						szSign = "+"
+#						if szYield < 0:
+#							szSign = ""
+#						szYield += u"<font=3>%s%i%c</font>" % (szSign, iYieldChange, gc.getYieldInfo(iYield).getChar())
+#				ltTerrainModifier.append( (iTerrain, szYield) )
+
+		#place panel with variable title
+
+		#place each buttons
+		for iTerrain, szYield in ltTerrainModifier:
+			childPanelName = self.top.getNextWidgetName()
+			screen.attachLabel(panelName, "", "  ")
+			screen.attachPanel(panelName, childPanelName, "", "", False, False, PanelStyles.PANEL_STYLE_EMPTY)
+			screen.attachImageButton(childPanelName, "", gc.getTerrainInfo(iTerrain).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_TERRAIN, iTerrain, 1, False)
+			screen.attachLabel(childPanelName, "", szYield)
+
+
+	def placeHero(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_PEDIA_CATEGORY_HERO", ()), "", False, True, self.X_HEROES, self.Y_HEROES, self.W_HEROES, self.H_HEROES, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.attachLabel(panelName, "", "  ")
+
+		iHero = gc.getCivilizationInfo(self.iCivilization).getHero()
+		if iHero != UnitTypes.NO_UNIT:
+			heroInfo = gc.getUnitInfo(iHero)
+			screen.attachImageButton(panelName, "", heroInfo.getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_UNIT, iHero, 1, False)
+
+
+
+	def placeWorldSpell(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_CONCEPT_WORLD_SPELLS", ()), "", False, True, self.X_SPELLS, self.Y_SPELLS, self.W_SPELLS, self.H_SPELLS, PanelStyles.PANEL_STYLE_BLUE50)
+		screen.attachLabel(panelName, "", "  ")
+
+		for iSpell in range(gc.getNumSpellInfos()):
+			spellInfo = gc.getSpellInfo(iSpell)
+			if spellInfo.isGlobal():
+				if spellInfo.getCivilizationPrereq() == self.iCivilization:
+					screen.attachImageButton(panelName, "", spellInfo.getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_SPELL, iSpell, 1, False)
+					break
+##--------	BUGFfH: End Add
 
 
 
--- python/Contrib/Sevopedia/SevoPediaTerrain.py
+++ python/Contrib/Sevopedia/SevoPediaTerrain.py
@@ -26,10 +26,31 @@ class SevoPediaTerrain:
 		self.iTerrain = -1
 		self.top = main
 
+##--------	BUGFfH: Added by Denev 2009/10/07
+		X_MERGIN = self.top.X_MERGIN
+		Y_MERGIN = self.top.Y_MERGIN
+
+		self.W_EARTHLY = 200
+		self.H_EARTHLY = 110
+		self.X_EARTHLY = self.top.R_PEDIA_PAGE - self.W_EARTHLY
+		self.Y_EARTHLY = self.top.Y_PEDIA_PAGE
+
+		self.X_HELL = self.X_EARTHLY
+		self.Y_HELL = self.Y_EARTHLY + self.H_EARTHLY + Y_MERGIN
+		self.W_HELL = self.W_EARTHLY
+		self.H_HELL = 110
+##--------	BUGFfH: End Add
+
 		self.X_ICON_PANE = self.top.X_PEDIA_PAGE
 		self.Y_ICON_PANE = self.top.Y_PEDIA_PAGE
+##--------	BUGFfH: Modified by Denev 2009/10/07
+		"""
 		self.W_ICON_PANE = self.top.W_PEDIA_PAGE
 		self.H_ICON_PANE = 210
+		"""
+		self.W_ICON_PANE = self.top.W_PEDIA_PAGE - self.W_EARTHLY - X_MERGIN
+		self.H_ICON_PANE = self.Y_HELL + self.H_HELL - self.Y_ICON_PANE
+##--------	BUGFfH: End Modify
 
 		self.W_ICON = 150
 		self.H_ICON = 150
@@ -37,13 +58,24 @@ class SevoPediaTerrain:
 		self.Y_ICON = self.Y_ICON_PANE + (self.H_ICON_PANE - self.H_ICON) / 2
 		self.ICON_SIZE = 64
 
+##--------	BUGFfH: Modified by Denev 2009/10/07
+		"""
 		self.X_STATS_PANE = self.X_ICON_PANE + 210
 		self.W_STATS_PANE = self.top.R_PEDIA_PAGE - self.X_STATS_PANE
 		self.Y_STATS_PANE = 130
 		self.H_STATS_PANE = 110
+		"""
+		self.X_STATS_PANE = self.X_ICON + self.W_ICON + 10
+		self.Y_STATS_PANE = self.Y_ICON
+		self.W_STATS_PANE = self.X_ICON_PANE + self.W_ICON_PANE - self.X_STATS_PANE
+		self.H_STATS_PANE = self.Y_ICON_PANE + self.H_ICON_PANE - self.Y_STATS_PANE
+##--------	BUGFfH: End Modify
 
 		self.X_SPECIAL_PANE = self.X_ICON_PANE
-		self.W_SPECIAL_PANE = self.W_ICON_PANE
+##--------	BUGFfH: Modified by Denev 2009/10/07
+#		self.W_SPECIAL_PANE = self.W_ICON_PANE
+		self.W_SPECIAL_PANE = self.top.W_PEDIA_PAGE
+##--------	BUGFfH: End Modify
 		self.Y_SPECIAL_PANE = self.Y_ICON_PANE + self.H_ICON_PANE + 10
 		self.H_SPECIAL_PANE = self.top.B_PEDIA_PAGE - self.Y_SPECIAL_PANE
 
@@ -52,12 +84,48 @@ class SevoPediaTerrain:
 	def interfaceScreen(self, iTerrain):
 		self.iTerrain = iTerrain
 		screen = self.top.getScreen()
+
+#BUGFfH: Added by Denev 2009/08/16
+		# Header...
+		szHeader = u"<font=4b>" + gc.getTerrainInfo(self.iTerrain).getDescription() + u"</font>"
+		szHeaderId = "PediaMainHeader"
+		screen.setText(szHeaderId, "Background", szHeader, CvUtil.FONT_CENTER_JUSTIFY, self.top.X_SCREEN, self.top.Y_TITLE, 0, FontTypes.TITLE_FONT, WidgetTypes.WIDGET_GENERAL, -1, -1)
+#BUGFfH: End Add
+
 		screen.addPanel(self.top.getNextWidgetName(), "", "", False, False, self.X_ICON_PANE, self.Y_ICON_PANE, self.W_ICON_PANE, self.H_ICON_PANE, PanelStyles.PANEL_STYLE_BLUE50)
 		screen.addPanel(self.top.getNextWidgetName(), "", "", False, False, self.X_ICON, self.Y_ICON, self.W_ICON, self.H_ICON, PanelStyles.PANEL_STYLE_MAIN)
 		screen.addDDSGFC(self.top.getNextWidgetName(), gc.getTerrainInfo(self.iTerrain).getButton(), self.X_ICON + self.W_ICON/2 - self.ICON_SIZE/2, self.Y_ICON + self.H_ICON/2 - self.ICON_SIZE/2, self.ICON_SIZE, self.ICON_SIZE, WidgetTypes.WIDGET_GENERAL, -1, -1)
 
 		self.placeStats()
 		self.placeSpecial()
+##--------	BUGFfH: Added by Denev 2009/10/07
+		self.placeEarthly()
+		self.placeHell()
+
+
+
+	def placeEarthly(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_PEDIA_TERRAIN_EARTHLY", ()), "", False, True, self.X_EARTHLY, self.Y_EARTHLY, self.W_EARTHLY, self.H_EARTHLY, PanelStyles.PANEL_STYLE_BLUE50)
+
+		iTerrainUp = gc.getTerrainInfo(self.iTerrain).getTerrainDown()
+		if iTerrainUp != TerrainTypes.NO_TERRAIN:
+			screen.attachLabel(panelName, "", "  ")
+			screen.attachImageButton(panelName, "", gc.getTerrainInfo(iTerrainUp).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_TERRAIN, iTerrainUp, 1, False)
+
+
+
+	def placeHell(self):
+		screen = self.top.getScreen()
+		panelName = self.top.getNextWidgetName()
+		screen.addPanel(panelName, localText.getText("TXT_KEY_PEDIA_TERRAIN_HELL", ()), "", False, True, self.X_HELL, self.Y_HELL, self.W_HELL, self.H_HELL, PanelStyles.PANEL_STYLE_BLUE50)
+
+		iTerrainDown = gc.getTerrainInfo(self.iTerrain).getTerrainUp()
+		if iTerrainDown != TerrainTypes.NO_TERRAIN:
+			screen.attachLabel(panelName, "", "  ")
+			screen.attachImageButton(panelName, "", gc.getTerrainInfo(iTerrainDown).getButton(), GenericButtonSizes.BUTTON_SIZE_CUSTOM, WidgetTypes.WIDGET_PEDIA_JUMP_TO_TERRAIN, iTerrainDown, 1, False)
+#BUGFfH: End Add
 
 
 
