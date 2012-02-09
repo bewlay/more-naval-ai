4dce0186f3f79cebe1954bbc05743c709e566447
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2012-02-09 04:42:30 +0000

    Bugfix - Leader promotions (ie Great Generals) will no longer be mirrored by the Mirror() function

--- python/entrypoints/CvSpellInterface.py
+++ python/entrypoints/CvSpellInterface.py
@@ -1869,9 +1869,10 @@ def spellMirror(caster):
 	pPlayer = gc.getPlayer(caster.getOwner())
 	newUnit = pPlayer.initUnit(caster.getUnitType(), caster.getX(), caster.getY(), UnitAITypes.NO_UNITAI, DirectionTypes.DIRECTION_SOUTH)
 	for i in range(gc.getNumPromotionInfos()):
-		newUnit.setHasPromotion(i, caster.isHasPromotion(i))
-		if gc.getPromotionInfo(i).isEquipment():
-			newUnit.setHasPromotion(i, False)
+		if ((not gc.getPromotionInfo(i).isLeader()) and (not gc.getPromotionInfo(i).isEquipment())):
+			newUnit.setHasPromotion(i, caster.isHasPromotion(i))
+#		if gc.getPromotionInfo(i).isEquipment():
+#			newUnit.setHasPromotion(i, False)
 	newUnit.setHasPromotion(gc.getInfoTypeForString('PROMOTION_ILLUSION'), True)
 	newUnit.setHasPromotion(gc.getInfoTypeForString('PROMOTION_IMMORTAL'), False)
 	if newUnit.isImmortal():
