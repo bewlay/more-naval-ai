ed42f9fd07599153e14ac9f421c220bd05b8968f
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2012-05-21 19:39:43 +0000

    Bugfix - using convert() to set religions for rebels (code was using setLastReligion() which was causing some issues. This should be better overall)

--- python/Revolution/Gameready/Revolution.py
+++ python/Revolution/Gameready/Revolution.py
@@ -4238,7 +4238,8 @@ class Revolution :
 		if( not giveRelType == None and not pRevPlayer.isAlive() and not pRevPlayer.isBarbarian() and not pRevPlayer.isAgnostic()) :
 			if( giveRelType >= 0 ) :
 				# Give specified religion
-				pRevPlayer.setLastStateReligion( giveRelType )
+#				pRevPlayer.setLastStateReligion( giveRelType )
+				pRevPlayer.convert( giveRelType )
 				if( self.LOG_DEBUG ) :
 					#CvUtil.pyPrint("  Revolt - Setting revolutionary's religion to %d"%(giveRelType))
 					religionDesc = gc.getReligionInfo(giveRelType).getDescription()
