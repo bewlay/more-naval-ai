1e0abdb574628c97e92626b7ca9bb9a559571159
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2012-12-21 15:27:34 +0000

    Updating to version 2.4

--- CvGameCoreDLL/CvDLLWidgetData.cpp
+++ CvGameCoreDLL/CvDLLWidgetData.cpp
@@ -5099,7 +5099,7 @@ void CvDLLWidgetData::parseFlagHelp(CvWidgetDataStruct &widgetDataStruct, CvWStr
 
 // More Naval AI version number
 	// Add string showing version number
-	szTempBuffer.Format(L"%S", "More Naval AI v2.3");
+	szTempBuffer.Format(L"%S", "More Naval AI v2.4");
 	szBuffer.append(szTempBuffer);
 	szBuffer.append(NEWLINE);
 // End More Naval AI
