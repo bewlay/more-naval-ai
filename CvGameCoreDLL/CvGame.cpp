1aacf9abaa80d81bfb0dcde6ac450ee0c880ef4f
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2013-09-24 19:54:42 +0000

    Bugfix - removing a block on the gameUpdate python call - fix for Civ4lerts not working ([bugs:#75])

--- CvGameCoreDLL/CvGame.cpp
+++ CvGameCoreDLL/CvGame.cpp
@@ -2426,18 +2426,9 @@ void CvGame::update()
 	{
 		sendPlayerOptions();
 
-//FfH: Modified by Kael 10/15/2008
-//		// sample generic event
-//		CyArgsList pyArgs;
-//		pyArgs.add(getTurnSlice());
-//		CvEventReporter::getInstance().genericEvent("gameUpdate", pyArgs.makeFunctionArgs());
-        if (isNetworkMultiPlayer())
-		{
-            CyArgsList pyArgs;
-            pyArgs.add(getTurnSlice());
-            CvEventReporter::getInstance().genericEvent("gameUpdate", pyArgs.makeFunctionArgs());
-        }
-//FfH: End Modify
+		CyArgsList pyArgs;
+		pyArgs.add(getTurnSlice());
+		CvEventReporter::getInstance().genericEvent("gameUpdate", pyArgs.makeFunctionArgs());
 
 		if (getTurnSlice() == 0)
 		{
