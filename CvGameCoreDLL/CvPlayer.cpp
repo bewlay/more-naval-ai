2bb616239049c32699e0a5793846fd497244de4d
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2013-10-11 16:11:33 +0000

    Code - code cleanup

--- CvGameCoreDLL/CvPlayer.cpp
+++ CvGameCoreDLL/CvPlayer.cpp
@@ -5496,29 +5496,33 @@ bool CvPlayer::canContact(PlayerTypes ePlayer) const
 		return false;
 	}
 
-	if (!isAlive() || !(GET_PLAYER(ePlayer).isAlive()))
+	CvPlayer &kPlayer = GET_PLAYER(ePlayer);
+
+	if (!isAlive() || !(kPlayer.isAlive()))
 	{
 		return false;
 	}
 
-	if (isBarbarian() || GET_PLAYER(ePlayer).isBarbarian())
+	if (isBarbarian() || kPlayer.isBarbarian())
 	{
 		return false;
 	}
 
-	if (isMinorCiv() || GET_PLAYER(ePlayer).isMinorCiv())
+	if (isMinorCiv() || kPlayer.isMinorCiv())
 	{
 		return false;
 	}
 
-	if (getTeam() != GET_PLAYER(ePlayer).getTeam())
+	if (getTeam() != kPlayer.getTeam())
 	{
-		if (!(GET_TEAM(getTeam()).isHasMet(GET_PLAYER(ePlayer).getTeam())))
+		CvTeam &kTeam = GET_TEAM(getTeam());
+
+		if (!(kTeam.isHasMet(kPlayer.getTeam())))
 		{
 			return false;
 		}
 
-		if (atWar(getTeam(), GET_PLAYER(ePlayer).getTeam()))
+		if (atWar(getTeam(), kPlayer.getTeam()))
 		{
 
 //FfH: Added by Kael 08/14/2007
@@ -5528,13 +5532,13 @@ bool CvPlayer::canContact(PlayerTypes ePlayer) const
             }
 //FfH: End Add
 
-			if (!(GET_TEAM(getTeam()).canChangeWarPeace(GET_PLAYER(ePlayer).getTeam())))
+			if (!(kTeam.canChangeWarPeace(kPlayer.getTeam())))
 			{
 				return false;
 			}
 		}
 
-		if (isHuman() || GET_PLAYER(ePlayer).isHuman())
+		if (isHuman() || kPlayer.isHuman())
 		{
 			if (GC.getGameINLINE().isOption(GAMEOPTION_ALWAYS_WAR))
 			{
