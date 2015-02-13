4c49f97b052fa6bfea4239381cdecf364bbf058c
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2015-02-13 21:42:24 +0000

    Bugfix - Heal spell will now work again (fix by fe79)

--- python/entrypoints/CvSpellInterface.py
+++ python/entrypoints/CvSpellInterface.py
@@ -1530,8 +1530,8 @@ def spellHeal(caster,amount):
 	pPlot = caster.plot()
 	iPoisoned = gc.getInfoTypeForString('PROMOTION_POISONED')
 	for i in range(pPlot.getNumUnits()):
+		pUnit = pPlot.getUnit(i)
 		if not pUnit.isImmuneToMagic():
-			pUnit = pPlot.getUnit(i)
 			pUnit.setHasPromotion(iPoisoned,False)
 			if pUnit.isAlive():
 				pUnit.changeDamage(-amount, PlayerTypes.NO_PLAYER)
