55becbbcb9d100ef542af9eb7c559f87558fc3ef
tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084 <tholal@ee96410b-0b5e-47b8-b08c-f50b2ea78084>
2015-03-16 20:05:44 +0000

    Bugfix - PuppetTradingCount will now be properly saved

--- CvGameCoreDLL/CvTeam.cpp
+++ CvGameCoreDLL/CvTeam.cpp
@@ -253,6 +253,7 @@ void CvTeam::reset(TeamTypes eID, bool bConstructorCall)
 	m_iDefensivePactTradingCount = 0;
 	m_iPermanentAllianceTradingCount = 0;
 	m_iVassalTradingCount = 0;
+	m_iPuppetTradingCount = 0; // MNAI - Puppet States
 	m_iBridgeBuildingCount = 0;
 	m_iIrrigationCount = 0;
 	m_iIgnoreIrrigationCount = 0;
@@ -7383,6 +7384,7 @@ void CvTeam::read(FDataStreamBase* pStream)
 	pStream->Read(&m_iDefensivePactTradingCount);
 	pStream->Read(&m_iPermanentAllianceTradingCount);
 	pStream->Read(&m_iVassalTradingCount);
+	pStream->Read(&m_iPuppetTradingCount); // MNAI - Puppet States
 	pStream->Read(&m_iBridgeBuildingCount);
 	pStream->Read(&m_iIrrigationCount);
 	pStream->Read(&m_iIgnoreIrrigationCount);
@@ -7510,6 +7512,7 @@ void CvTeam::write(FDataStreamBase* pStream)
 	pStream->Write(m_iDefensivePactTradingCount);
 	pStream->Write(m_iPermanentAllianceTradingCount);
 	pStream->Write(m_iVassalTradingCount);
+	pStream->Write(m_iPuppetTradingCount); // MNAI - Puppet States
 	pStream->Write(m_iBridgeBuildingCount);
 	pStream->Write(m_iIrrigationCount);
 	pStream->Write(m_iIgnoreIrrigationCount);
