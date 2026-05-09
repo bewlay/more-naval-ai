e474f794bdb718865cdce3246c5210c98d79de88
lfgr <5755046+bewlay@users.noreply.github.com>
2026-05-09 20:51:43 +0200

    Bugfix - Change duplicate player names in new games, not just for revolutions or similar (Two players should never have the same name to prevent ambiguity, e.g., in diplomacy.)

--- python/FfH/FfHNames.py
+++ python/FfH/FfHNames.py
@@ -9,23 +9,37 @@ gc = CyGlobalContext()
 
 def onSetPlayerAlive( args ) :
 	ePlayer, bNewValue = args
-	print( "SET_PLAYER_ALIVE" )
 
 	if bNewValue :
 		setNewLeaderName( ePlayer )
 
+# onSetPlayerAlive is not called on game start, since our BUG module isn't registered yet
+def onGameStart( _argsList ) :
+	for ePlayer in xrange( gc.getMAX_CIV_PLAYERS() ) :
+		if gc.getPlayer( ePlayer ).isAlive() :
+			# Set bPrecedence = True to rename players in order
+			setNewLeaderName( ePlayer, bPrecedence = True )
 
-def setNewLeaderName( ePlayer ) :
-	""" Rename the given player, if necessary/desired """
+def setNewLeaderName( ePlayer, bPrecedence = False ) :
+	"""
+	Rename the given player, if necessary due to duplicates.
+	If bPrecedence=True, only check lower-index players for duplicates.
+	"""
 	pPlayer = gc.getPlayer( ePlayer ) # type: CyPlayer
 	szName = pPlayer.getName()
+	
+	# Find other player's names
+	if bPrecedence :
+		iMaxPlayer = ePlayer
+	else :
+		iMaxPlayer = gc.getMAX_CIV_PLAYERS()
+	hszOtherNames = set( gc.getPlayer( eLoopPlayer ).getName() for eLoopPlayer in range( iMaxPlayer )
+			if eLoopPlayer != ePlayer and gc.getPlayer( eLoopPlayer ).isAlive() )
 
 	# Add roman numerals in case there are duplicates
-	hszNames = set( gc.getPlayer( eLoopPlayer ).getName() for eLoopPlayer in range( gc.getMAX_CIV_PLAYERS() )
-			if eLoopPlayer != ePlayer and gc.getPlayer( eLoopPlayer ).isAlive() )
 	szNewName = szName
 	iCount = 1
-	while szNewName in hszNames :
+	while szNewName in hszOtherNames :
 		iCount += 1
 		szNewName = szName + u" " + unicode( Roman.toRoman( iCount ) )
 	pPlayer.setName( szNewName )
